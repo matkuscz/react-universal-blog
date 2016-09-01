8aca3600300e7cd356b9aeb2e2ea37da51df4e44
Tomas Matejka <mtk.eworkers@gmail.com>
2016-09-01 22:07:33 +0200

    Prvni nahozeni

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+// Based on https://www.sitepoint.com/building-a-react-universal-blog-app-a-step-by-step-guide/
+// Windows env variables:
+// to define type "set node_env=production" in plain cmd.exe, not tested in PowerShell
+// to list all env vars type "set"
+require('babel/register')
+require('./app-server.js')
---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+export default {
+  site: {
+    title: 'React Universal Blog'
+  },
+  bucket: {
+    //slug: 'react-universal-blog',
+    slug: 'https://api.cosmicjs.com/v1/mtkuniversalblog?pretty=true',
+    media_url: 'https://cosmicjs.com/uploads'
+  }
+}
